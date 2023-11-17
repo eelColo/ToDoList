b4f33523fe6cf6e7eace41a1ec13380c281b1740
Franco <francolens2002@gmail.com>
2023-11-17 16:15:10 -0300

    Sumando funcionalidad a Agregar y eliminar

--- com.todolist/src/main/java/com/todolistbackend/com/todolist/controller/MainController.java
+++ com.todolist/src/main/java/com/todolistbackend/com/todolist/controller/MainController.java
@@ -1,19 +1,24 @@
 package com.todolistbackend.com.todolist.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.todolistbackend.com.todolist.entity.Task;
 import com.todolistbackend.com.todolist.service.TaskService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/rest")
@@ -25,8 +30,14 @@ public class MainController {
 	//Primero crear
 	
 	@PostMapping("/task")
-	public void createTask(@RequestBody Task task) {
+	public void createTask(@ModelAttribute("ntask") @RequestBody Task task, HttpServletResponse res ) {
 		taskServ.createTask(task);
+		try {
+			res.sendRedirect("/todolist");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	//Listar todas
@@ -47,9 +58,20 @@ public class MainController {
 	public void editTask(@PathVariable(value = "id") Integer idTask,@RequestBody Task task) {
 		taskServ.editTask(idTask, task);
 	}
-	@DeleteMapping("/task/{id}")
-	public void deleteTask(@PathVariable(value = "id") Integer idTask) {
+	
+	//Borrado
+	@PostMapping("/task/{id}")
+	public void deleteTask(@PathVariable(value = "id") int idTask, HttpServletResponse res) {
+		
 		taskServ.deleteTaskById(idTask);
+		
+		
+		try {
+			res.sendRedirect("/todolist");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
